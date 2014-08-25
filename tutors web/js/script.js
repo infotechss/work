fe1b53919dbf044c4fae6b2b47cf35bf61fec76d
Haroon <haroonm@live.com>
2014-08-25 21:07:56 +0500

    Student Module completed for iPad and up

--- tutors web/js/script.js
+++ tutors web/js/script.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
 		$("#right-panel").animate({ right:'-223px' }, 500);
 	return false;
 	})
+
+	//$("div").removeClass("logged-in");
+	
+	var $scrollbar = $("#scrollbar1");
+	$scrollbar.tinyscrollbar();
+	$(".sb-collapse").click(function(){
+		$(".study-right").hide();
+		return false;
+	})
 })

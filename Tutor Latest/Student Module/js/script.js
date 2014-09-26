6045ba28abe998c6534c75e424c33eb37d761c49
Umair Amin <webpresence000@gmail.com>
2014-09-26 20:22:24 +0500

    Right panel implementation in company module

---
+++ Tutor Latest/Student Module/js/script.js
@@ -0,0 +1,36 @@
+
+jQuery.noConflict(); 
+var j$ = jQuery;
+
+j$(document).ready(function(){
+	j$("#panel-btn").click(function(){
+		j$("#right-panel").animate({ right:'0' }, 500);
+	return false;
+	})
+	j$(".close-btn").click(function(){
+		j$("#right-panel").animate({ right:'-223px' }, 500);
+	return false;
+	})
+
+	j$(".exit-btn").click(function(){
+		j$(".math-banner").hide(800);
+		return false;
+	})
+
+	j$(".navbar-toggle").click(function(){
+		j$("nav").slideToggle();
+	return false;
+	})
+
+	j$( ".sb-collapse a" ).click(function() {
+		j$( "#study-panel, #study-video" ).toggleClass( "newClass", 500 );
+	});
+
+	j$("[data-toggle=tooltip]").tooltip();
+
+	// j$(".logged-in").removeClass("logged-in");
+	
+	var j$scrollbar = j$("#scrollbar1");
+	j$scrollbar.tinyscrollbar();
+
+})
---
+++ Tutor Latest/Tutor Module/js/script.js
@@ -0,0 +1,67 @@
+
+jQuery.noConflict(); 
+var j$ = jQuery;
+
+j$(document).ready(function(){
+	j$("#panel-btn").click(function(){
+		j$("#right-panel").animate({ right:'0' }, 500);
+	return false;
+	})
+
+	j$(".close-btn").click(function(){
+		j$("#right-panel").animate({ right:'-223px' }, 500);
+	return false;
+	})
+
+	j$(".navbar-toggle").click(function(){
+		j$("nav").slideToggle();
+	return false;
+	})
+
+	/*	FAQ popup	*/
+	j$(".faq-popup a").click(function(){
+		if ( j$(this).parent().hasClass('active') ) {
+			j$(".faq-popup li").removeClass("active");
+			j$(".faq-info").slideUp();
+			j$("this").children(".right-icon").removeClass("fa-chevron-circle-down");
+			j$(this).children(".right-icon").addClass("fa-chevron-circle-right");
+
+		}
+		else{
+			j$(".faq-popup li").removeClass("active");
+			j$(".faq-info").slideUp();
+			j$(".faq-popup a").children(".right-icon").removeClass("fa-chevron-circle-down");
+			j$(this).parent().addClass("active");
+			j$(this).next('.faq-info').slideDown();
+			j$(this).parent().addClass("active");
+			j$(this).children(".right-icon").removeClass("fa-chevron-circle-down");
+			j$(this).children(".right-icon").addClass("fa-chevron-circle-down");
+		}
+				
+		return false;
+	})
+
+	j$(".exit-btn").click(function(){
+		j$(".tutor-banner").hide(800);
+		return false;
+	})
+
+	j$(".email-btn").click(function(){
+		j$(".contact-us").fadeToggle();
+		j$(this).toggleClass("active");
+		return false;
+	})
+
+
+
+	// j$(".logged-in").removeClass("logged-in");
+	
+	var j$scrollbar = j$("#scrollbar1");
+	j$scrollbar.tinyscrollbar();
+	j$(".sb-collapse").click(function(){
+		j$(".study-right").hide();
+		return false;
+	})
+
+
+})
